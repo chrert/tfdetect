56f8a8b49f4cb3d0f326e23a0db2fedbe88a6de5
Christian Ertler <christian.ertler@icg.tugraz.at>
2017-06-23 13:52:57 +0200

    add cmake scripts to build the tensorflow c_api

---
+++ c_api/cmake/build_tensorflow.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+# configure environmental variables
+export CC_OPT_FLAGS="-march=native"
+export TF_NEED_GCP=0
+export TF_NEED_HDFS=0
+export TF_NEED_OPENCL=0
+export TF_NEED_JEMALLOC=1
+export TF_NEED_VERBS=0
+export TF_NEED_MKL=1
+export TF_DOWNLOAD_MKL=1
+export TF_NEED_MPI=0
+export TF_ENABLE_XLA=1
+export TF_CUDA_CLANG=0
+export TF_NEED_CUDA=0
+# TODO: export correct python paths
+
+
+# configure and build
+./configure
+bazel build -c opt --copt=${CC_OPT_FLAGS} tensorflow:libtensorflow.so
+bazel shutdown
